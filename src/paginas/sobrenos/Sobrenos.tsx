01c1592794184e4e7d15b7bfc7fd6a890021d1bc
natanael <natanaelpaes32@gmail.com>
2023-04-14 15:53:23 -0300

    "coraçao navbar"

--- src/paginas/sobrenos/Sobrenos.tsx
+++ src/paginas/sobrenos/Sobrenos.tsx
@@ -2,16 +2,17 @@ import { Box, Button, Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Sobrenos.css'
+import { Opacity } from '@material-ui/icons'
 
 function Sobrenos() {
   return (
     <>
         <Grid container display={'flex'} justifyContent={'center'} className='fotoSobrenos'>
-            <Grid xs={6} paddingTop={3} style={{ color: 'white' }}>
+            <Grid xs={6} paddingTop={3} style={{ color: '#0A0840'}}>
                 
                 <Box paddingX={8} justifyContent={'start'}>
                 <Typography variant='h2' gutterBottom justifyContent={'start'} align='center'>Sobre nós</Typography>
-                    <Typography marginTop={5} variant='h6' justifyContent={'center'} align='center'>
+                    <Typography marginTop={5} variant='h6' justifyContent={'center'} align='center'style={{background: '#EBF2F299'}}>
                         O projeto ConectaVaga é uma rede social desenvolvida com o intuito de 
                         promover a visibilidade, inclusão e senso de pertencimento a grupos 
                         minoritários que estejam em situação de vulnerabilidade social ou econômica. 
