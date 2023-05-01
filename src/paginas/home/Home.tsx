da8bb59c065e2b3eb7cf751ee892562d7cf4cad8
MuRibeiro <mucostaribeiro@gmail.com>
2023-05-01 20:08:19 -0300

    Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,11 +5,11 @@ import { Box } from "@mui/material"
 import Typography from '@mui/material/Typography';
 import { Link, useNavigate } from 'react-router-dom';
 import ListaPostagem from "../../components/postagens/listaPostagem/ListaPostagem";
-import ListaTema from "../../components/temas/listaTema/ListaTema";
 import { TokenState } from "../../store/tokens/TokensReducer";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+
 
 function Home() {
 
@@ -39,27 +39,31 @@ function Home() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-                <Grid alignItems="center" item xs={6}>
+                <Grid item xs={12}>
+                    <img src="https://ik.imagekit.io/6kg1q0s1r/C%C3%B3pia_de_Banners.png?updatedAt=1681922292947" alt="Imagem Capa" className="fotoHome" />
+                </Grid>
+                <Grid alignItems="center" item xs={12}>
                     <Box paddingX={12}>
-                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>
-                            Seja bem-vindo!
+                        <Typography variant="h4" component="h3" align="center" className='titulo'>
+                            Postagens em Destaques
                         </Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>
+                        <Typography variant="subtitle1" gutterBottom component="h3" align="center" className='subtitulo'>
                             O que gostaria de compartilhar?
                         </Typography>
                     </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
+                    <Box display="flex" justifyContent="center" marginBottom={6}> 
+                        <Box display={"flex"}>
                             <ModalPostagem />
                         </Box>
+                        <Box marginLeft={3}>   
                             <Link to={"/postagens"}>
-                                <Button variant="outlined" className='botao'> Ver Postagens</Button>
+                                <Button variant="outlined" style={{backgroundColor:"#1d3557"}}> 
+                                    Ver Postagens
+                                </Button>
                             </Link>
+                        </Box>
                     </Box>
                 </Grid>
-                {/* <Grid item xs={6}>
-                    <img src="https://ik.imagekit.io/6kg1q0s1r/Writer_s_block-amico.png?updatedAt=1681323288251" alt="Imagem Capa" className="fotoHome" />
-                </Grid> */}
                 <Grid xs={12} className='postagens'>
                     <ListaPostagem />
                 </Grid>
