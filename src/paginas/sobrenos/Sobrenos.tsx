20623c351e10fe25a7aa993decb16bf6a9977b72
Joaopanzo261 <joaopanzo261@gmail.com>
2023-05-02 10:32:15 -0300

    Sobre nós com valores,missão e propósito

--- src/paginas/sobrenos/Sobrenos.tsx
+++ src/paginas/sobrenos/Sobrenos.tsx
@@ -3,28 +3,44 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Sobrenos.css'
 import { Opacity } from '@material-ui/icons'
+import Carousel from '../../components/carousel/Carousel'
 
 function Sobrenos() {
   return (
     <>
-        <Grid container display={'flex'} justifyContent={'center'} className='fotoSobrenos'>
+        <Grid container display={'flex'} justifyContent={'center'}>
             <Grid xs={6} paddingTop={3} style={{ color: '#0A0840'}}>
                 
-                <Box paddingX={8} justifyContent={'start'}>
-                <Typography variant='h2' gutterBottom justifyContent={'start'} align='center'>Sobre nós</Typography>
-                    <Typography marginTop={5} variant='h6' justifyContent={'center'} align='center'style={{background: '#EBF2F299'}}>
-                        O projeto ConectaVaga é uma rede social desenvolvida com o intuito de 
-                        promover a visibilidade, inclusão e senso de pertencimento a grupos 
-                        minoritários que estejam em situação de vulnerabilidade social ou econômica. 
-                        Buscando impactar positivamente o desenvolvimento pessoal e profissional dos 
-                        nossos usuários, através da divulgação de oportunidades de trabalho e 
-                        capacitação, visando assim torná-los economicamente ativos e protagonistas 
-                        de suas carreiras.
+                <Box paddingX={8} justifyContent={'center'}>
+                <Typography variant='h2' gutterBottom justifyContent={'center'} align='center'>Quem somos</Typography>
+                    <Typography marginTop={5} variant='h6' justifyContent={'center'} align='center'style={{background: ''}} >
+
+                    <img src='https://cdn.discordapp.com/attachments/1070726708614934629/1101574421568831529/imagem_2023-04-28_152318226-removebg-preview.png' alt='Conexão' />
+                       <br></br>
+                              O projeto ConectaVaga é uma rede social desenvolvida com o intuito de promover a visibilidade, inclusão
+                              e senso de pertencimento a grupos minoritários que estejam em situação de vulnerabilidade social ou econômica. 
+                              Buscando impactar positivamente o desenvolvimento pessoal e profissional dos nossos usuários, através da divulgação 
+                              de oportunidades de trabalho e capacitação, visando assim torná-los economicamente ativos e protagonistas de suas carreiras.
+
+                              <br></br>
+                              <strong>Valores</strong>: baseado na ética e responsabilidade social, valorizamos as diferenças e acreditamos em oportunidades iguais.
+
+                               <br></br>
+                              <strong> Missão</strong>: no ambiente digital, ser o meio que conecta pessoas de todos os contextos de vulnerabilidades a oportunidades de trabalho.
+
+                              <br></br>
+                              <strong>Propósito</strong>: contribuir na construção de um futuro próspero para as pessoas, e um progresso para todos nossos usuários.
+
+                              
+
                     </Typography>
-                </Box>            
-            </Grid>
-            
+                   
+                </Box>   
+                        
+            </Grid>   
         </Grid>
+
+       
     </>
   )
 }
